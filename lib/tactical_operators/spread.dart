2737dc3adc109c258efddb25611cc0a520698495
Diogo Sequeira <diogosequeira@hotmail.fr>
2021-02-20 16:21:54 +0000

    adds examples of list spread and without spread

--- lib/tactical_operators/spread.dart
+++ lib/tactical_operators/spread.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 
-List<int> joinCollectionsWithoutSpread() {}
+List<int> joinCollectionsWithoutSpread() {
+  final collection = [1, 2, 3];
+  final otherCollection = [4, 5, 6];
+  collection.addAll(otherCollection);
+  return collection;
+}
 
-List<int> joinCollectionsWithSpread() {}
+List<int> joinCollectionsWithSpread() {
+  /// Unpacks the elements of a collection so they can be used in another collection
+  final collection = [1, 2, 3];
+  final otherCollection = [4, 5, 6];
+  return [...collection, ...otherCollection];
+}
 
 class MySpreadWidget extends StatelessWidget {
   @override
