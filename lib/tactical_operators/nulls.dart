95c05401deeca921a0c4cd7c1efc8059a264f366
Diogo Sequeira <diogosequeira@hotmail.fr>
2021-02-20 16:59:31 +0000

    adds null operator examples

--- lib/tactical_operators/nulls.dart
+++ lib/tactical_operators/nulls.dart
@@ -1,12 +1,56 @@
 import 'package:flutter/material.dart';
 
-void withoutCoalescing() {}
+void withoutCoalescing() {
+  String myValue;
+  String fallbackValue = 'fallback';
 
-void withCoalescing() {}
+  String result;
+  if (myValue == null)
+    result = fallbackValue;
+  else
+    result = myValue;
 
-void assignment() {}
+  print(result); // it will be fallback
 
-void access() {}
+  // You could do it with ternary
+
+  final resultTernary = myValue == null ? fallbackValue : myValue;
+
+  print(resultTernary); // it will be fallback
+}
+
+void withCoalescing() {
+  String myValue;
+  String fallbackValue = 'fallback';
+
+  final result = myValue ?? fallbackValue;
+
+  print(result); // it will be fallback
+}
+
+void assignment() {
+  String uninitializedValue;
+  String initializedValue = 'something';
+
+  // Null-aware assignment operator, only assigned if null.
+  uninitializedValue ??= 'new value';
+  initializedValue ??= 'new value';
+
+  print(uninitializedValue); // Prints new value
+  print(initializedValue); // Remains something
+}
+
+void access() {
+  // Null-safe access operator
+  String uninitializedValue;
+  String initializedValue = 'something';
+
+  print(uninitializedValue.toUpperCase()); // Will throw null-pointer exception
+
+  print(uninitializedValue?.toUpperCase()); // Will print null.
+
+  print(initializedValue?.toUpperCase()); // Will print SOMETHING.
+}
 
 void spread() {}
 
