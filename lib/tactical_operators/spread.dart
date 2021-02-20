cc7dcd9687dc7c6068c1ec22d2e4653f33dc5f84
Diogo Sequeira <diogosequeira@hotmail.fr>
2021-02-20 16:17:45 +0000

    tactical operators scaffolding

---
+++ lib/tactical_operators/spread.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+List<int> joinCollectionsWithoutSpread() {}
+
+List<int> joinCollectionsWithSpread() {}
+
+class MySpreadWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
